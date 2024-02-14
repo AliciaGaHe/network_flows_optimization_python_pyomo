987f7a3cd5085c451298e5ae8fad5a3a1945126e
Jose Prieto <agarhernan@gmail.com>
2024-02-14 14:29:31 +0100

    sensibility analysis update

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ def create_model():
     # Import constraint dual information for sensibility analysis
     model.dual = Suffix(direction=Suffix.IMPORT)
 
+    # Import variable reduced cost information for sensibility analysis
+    model.rc = Suffix(direction=Suffix.IMPORT)
+
     # Create sets
     # Sources
     model.sSources = Set()
@@ -90,7 +93,8 @@ def create_model():
         """minimum total transportation cost"""
         return (
                 sum(
-                    model.pTransportationCosts[iSource, iCustomer] * model.vQuantityExchangedSourceCustomer[iSource, iCustomer]
+                    model.pTransportationCosts[iSource, iCustomer] *
+                    model.vQuantityExchangedSourceCustomer[iSource, iCustomer]
                     for iSource in model.sSources
                     for iCustomer in model.sCustomers
                     if (iSource, iCustomer) in model.sSources_sCustomers
@@ -129,20 +133,46 @@ def load_data_json_format(data_file):
     return data
 
 
-def print_conclusions_sources_sensibility_analysis(reduced_cost, source):
+def print_conclusions_sources_sensibility_analysis(shadow_price, source):
     """print conclusions of the source sensibility analysis"""
-    if reduced_cost < 0:
+    if shadow_price < 0:
         print("The total transportation cost would be reduced by",
-              abs(reduced_cost),
+              abs(shadow_price),
               "euros for each additional ton available in", source)
+    elif shadow_price > 0:
+        print("The total transportation cost would be increased in",
+              shadow_price,
+              "euros for each additional ton available in", source)
+    else:
+        print("The total transportation cost would remain equal for each additional ton available in", source)
 
 
 def print_conclusions_customers_sensibility_analysis(shadow_price, customer):
     """print conclusions of the customer sensibility analysis"""
-    if shadow_price > 0:
+    if shadow_price < 0:
+        print("The total transportation cost would be reduced by",
+              abs(shadow_price),
+              "euros for each additional ton supply at", customer)
+    elif shadow_price > 0:
         print("The total transportation cost would be increased in",
               shadow_price,
-              "euros for each additional ton demanded in", customer)
+              "euros for each additional ton supply at", customer)
+    else:
+        print("The total transportation cost would remain equal for each additional ton supply at", customer)
+
+
+def print_conclusions_routes_sensibility_analysis(reduced_cost, source, customer):
+    """print conclusions of the routes sensibility analysis"""
+    if reduced_cost < 0:
+        print("The total transportation cost would be reduced by",
+              abs(reduced_cost),
+              "euros for each ton supply from", source, "to", customer)
+    elif reduced_cost > 0:
+        print("The total cost would be increased in",
+              reduced_cost,
+              "euros for each ton supply from", source, "to", customer)
+    else:
+        print("The total transportation cost would remain equal for each ton supply from", source, "to", customer)
 
 
 def solve_problem_and_print_results(data_file):
@@ -178,44 +208,92 @@ def solve_problem_and_print_results(data_file):
         print("Total transportation cost: ", instance.f_obj(), "\n")
 
         print("Quantity exchanged between sources and customers:")
-        dict_quantity_sources_customers = {(iSource, iCustomer): value(instance.vQuantityExchangedSourceCustomer[iSource, iCustomer])
-                                           for iSource in instance.sSources
-                                           for iCustomer in instance.sCustomers
-                                           if (iSource, iCustomer) in instance.sSources_sCustomers and value(instance.vQuantityExchangedSourceCustomer[iSource, iCustomer]) > 0
-                                           }
-        df_quantity_sources_customers = pd.DataFrame(dict_quantity_sources_customers.values(),
-                                                     index=pd.MultiIndex.from_tuples(dict_quantity_sources_customers.keys()),
-                                                     columns=['Quantity']
-                                                     ).reset_index(names=['Source', 'Customer'])
+        dict_quantity_sources_customers = {
+            (iSource, iCustomer): value(instance.vQuantityExchangedSourceCustomer[iSource, iCustomer])
+            for iSource in instance.sSources
+            for iCustomer in instance.sCustomers
+            if (iSource, iCustomer) in instance.sSources_sCustomers and
+            value(instance.vQuantityExchangedSourceCustomer[iSource, iCustomer]) > 0
+        }
+        df_quantity_sources_customers = pd.DataFrame(
+            dict_quantity_sources_customers.values(),
+            index=pd.MultiIndex.from_tuples(dict_quantity_sources_customers.keys()),
+            columns=['Quantity']
+        ).reset_index(names=['Source', 'Customer'])
         print(df_quantity_sources_customers)
         print("\n")
 
         print("Sources sensibility analysis:")
-        dict_source_sensibility_analysis = {iSource: [value(instance.pSourceProduction[iSource]), value(instance.c01_production_limit_satisfaction[iSource]), value(instance.dual[instance.c01_production_limit_satisfaction[iSource]])]
-                                            for iSource in instance.sSources
-                                            }
-        df_source_sensibility_analysis = pd.DataFrame(dict_source_sensibility_analysis.values(),
-                                                      index=pd.Index(dict_source_sensibility_analysis.keys()),
-                                                      columns=['Capacity', 'Shipped', 'Reduced Cost']
-                                                      ).reset_index(names=['Source'])
+        dict_source_sensibility_analysis = {
+            iSource: [
+                value(instance.pSourceProduction[iSource]),
+                value(instance.c01_production_limit_satisfaction[iSource]),
+                value(instance.dual[instance.c01_production_limit_satisfaction[iSource]])
+            ]
+            for iSource in instance.sSources
+        }
+        df_source_sensibility_analysis = pd.DataFrame(
+            dict_source_sensibility_analysis.values(),
+            index=pd.Index(dict_source_sensibility_analysis.keys()),
+            columns=['Capacity', 'Shipped', 'Shadow price']
+            ).reset_index(names=['Source'])
         print(df_source_sensibility_analysis)
 
-        df_source_sensibility_analysis.apply(
-            lambda row: print_conclusions_sources_sensibility_analysis(row['Reduced Cost'], row['Source']), axis=1)
+        # Conclusions for resources without slack
+        df_source_sensibility_analysis[
+            df_source_sensibility_analysis['Capacity'] == df_source_sensibility_analysis['Shipped']
+        ].apply(
+            lambda row:
+            print_conclusions_sources_sensibility_analysis(row['Shadow price'], row['Source']), axis=1)
         print("\n")
 
         print("Customers sensibility analysis:")
-        dict_customers_sensibility_analysis = {iCustomer: [value(instance.pCustomerDemand[iCustomer]), value(instance.c02_demand_limit_satisfaction[iCustomer]), value(instance.dual[instance.c02_demand_limit_satisfaction[iCustomer]])]
-                                               for iCustomer in instance.sCustomers
-                                               }
-        df_customers_sensibility_analysis = pd.DataFrame(dict_customers_sensibility_analysis.values(),
-                                                         index=pd.Index(dict_customers_sensibility_analysis.keys()),
-                                                         columns=['Demand', 'Shipped', 'Shadow price']
-                                                         ).reset_index(names=['Customer'])
+        dict_customers_sensibility_analysis = {
+            iCustomer: [
+                value(instance.pCustomerDemand[iCustomer]),
+                value(instance.c02_demand_limit_satisfaction[iCustomer]),
+                value(instance.dual[instance.c02_demand_limit_satisfaction[iCustomer]])
+            ]
+            for iCustomer in instance.sCustomers
+        }
+        df_customers_sensibility_analysis = pd.DataFrame(
+            dict_customers_sensibility_analysis.values(),
+            index=pd.Index(dict_customers_sensibility_analysis.keys()),
+            columns=['Demand', 'Shipped', 'Shadow price']
+            ).reset_index(names=['Customer'])
         print(df_customers_sensibility_analysis)
 
-        df_customers_sensibility_analysis.apply(
-            lambda row: print_conclusions_customers_sensibility_analysis(row['Shadow price'], row['Customer']), axis=1)
+        # Conclusions for customer margin
+        df_customers_sensibility_analysis[
+            df_customers_sensibility_analysis['Demand'] == df_customers_sensibility_analysis['Shipped']
+        ].apply(
+            lambda row:
+            print_conclusions_customers_sensibility_analysis(row['Shadow price'], row['Customer']), axis=1)
+        print("\n")
+
+        print("Routes sensibility analysis:")
+        dict_routes_sensibility_analysis = {
+            (iSource, iCustomer): [
+                value(instance.vQuantityExchangedSourceCustomer[iSource, iCustomer]),
+                value(instance.rc[instance.vQuantityExchangedSourceCustomer[iSource, iCustomer]])
+            ]
+            for iSource in instance.sSources
+            for iCustomer in instance.sCustomers
+            if (iSource, iCustomer) in instance.sSources_sCustomers
+        }
+        df_routes_sensibility_analysis = pd.DataFrame(
+            dict_routes_sensibility_analysis.values(),
+            index=pd.MultiIndex.from_tuples(dict_routes_sensibility_analysis.keys()),
+            columns=['Quantity', 'Reduced cost']
+        ).reset_index(names=['Source', 'Customer'])
+        print(df_routes_sensibility_analysis)
+
+        # Conclusions for routes without quantity
+        df_routes_sensibility_analysis[
+            df_routes_sensibility_analysis['Quantity'] == 0
+        ].apply(
+            lambda row:
+            print_conclusions_routes_sensibility_analysis(row['Reduced cost'], row['Source'], row['Customer']), axis=1)
         print("\n")
 
     # If the solution in infeasible...
@@ -225,6 +303,7 @@ def solve_problem_and_print_results(data_file):
         print("Termination condition: ", results.solver.termination_condition)
         print("\n")
         print(results.write())
+
     # If something else is wrong...
     else:
         print("\n")
@@ -235,27 +314,27 @@ def solve_problem_and_print_results(data_file):
 # Solve some transportation problems
 
 # Base case
-solve_problem_and_print_results("data_0.json")
+solve_problem_and_print_results("./data/data_0.json")
 
 # Sensibility analysis - sources
-# Using base case, we move one ton of supply capacity from Gou to Arn and
-# the objetive function improves in 0.2 euros (reduced cost for Arn)
-# solve_problem_and_print_results("data_1.json")
+# Using the base case, we move one ton of supply capacity from Gou to Arn and
+# the objetive function improves in 0.2 euros (shadow price for Arn in the base case)
+# solve_problem_and_print_results("./data/data_1.json")
 
 # Sensibility analysis - customers - 1
-# Using base case, we increase the demand in Lon in one ton and
+# Using the base case, we increase the demand in Lon in one ton, and
 # we increase one ton of supply capacity in Gou (Gou is the only source for Lon).
-# Then the objetive function gets worse in 2.5 euros (shadow price for Lon)
-# solve_problem_and_print_results("data_2.json")
+# Then the objetive function gets worse in 2.5 euros (shadow price for Lon in the base case)
+# solve_problem_and_print_results("./data/data_2.json")
 
 # Sensibility analysis - customers - 2
-# Using base case, we increase the demand in Ber in one ton and
+# Using the base case, we increase the demand in Ber in one ton, and
 # we increase one ton of supply capacity in Gou (Arn is the only source for Ber).
-# Then the objetive function gets worse in 2.7 euros (shadow price for Ber)
-# solve_problem_and_print_results("data_3.json")
+# Then the objetive function gets worse in 2.7 euros (shadow price for Ber in the base case)
+# solve_problem_and_print_results("./data/data_3.json")
 
-# Using the constraint c03_fixed_transportations
-# Using base case, we fixed a transportation between Arn and Utr equal to 149 tons.
-# The flows between Arn and Ams/Utr and Gou and Ams/Utr change, as well as the objective function.
-# As we are doing something different of the base case (where all the flows are free), the solution is worse.
-# solve_problem_and_print_results("data_4.json")
+# Sensibility analysis - routes
+# Using the base case, we fixed a transportation between Arn and Ams equal to 1 ton,
+# using pFixedTransportation and c03_fixed_transportations.
+# The objetive function gets worse in 0.6 euros (reduced cost for the transportation between Arn and Ams)
+# solve_problem_and_print_results("./data/data_4.json")
